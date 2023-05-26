b16d65bb6aef9030ca98aadf21c43578217806d6
Denis Lemire <denis@lemire.name>
2023-05-26 12:28:53 -0600

    Revert random question code, it's broken. Fix later

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,6 +1,8 @@
 // Trivia questions and answers
 var triviaData;
 
+var triviaData;
+
 // Function to load the JSON file
 function loadJSON(callback) {
 	var xobj = new XMLHttpRequest();
@@ -17,58 +19,50 @@ function loadJSON(callback) {
 // Load the JSON file and assign the data to the triviaData variable
 loadJSON(function(data) {
 	triviaData = data;
+	displayQuestion();
 });
 
 var currentQuestion = 0;
 var score = 0;
-var totalQuestions = 0;
 
-function initializeQuiz(numberOfQuestions) {
-	totalQuestions = triviaData.length;
-	displayQuestion(numberOfQuestions);
-}
-
-function displayQuestion(n) {
+function displayQuestion() {
 	var questionContainer = document.getElementById("question");
 	var optionsContainer = document.getElementById("options");
+	var questionData = triviaData[currentQuestion];
 
-	if (currentQuestion < totalQuestions) {
-		var questionData = triviaData[currentQuestion];
-		questionContainer.innerHTML = questionData.question;
-		optionsContainer.innerHTML = "";
+	questionContainer.innerHTML = questionData.question;
+	optionsContainer.innerHTML = "";
 
-		for (var i = 0; i < questionData.options.length; i++) {
-			var option = document.createElement("button");
-			option.innerHTML = questionData.options[i];
-			option.addEventListener("click", checkAnswer);
-			optionsContainer.appendChild(option);
-		}
+	for (var i = 0; i < questionData.options.length; i++) {
+		var option = document.createElement("button");
+		option.innerHTML = questionData.options[i];
+		option.addEventListener("click", checkAnswer);
+		optionsContainer.appendChild(option);
 	}
 }
 
 function checkAnswer(event) {
-	var selectedOption = event.target;
-	var answer = triviaData[currentQuestion].answer;
+    var selectedOption = event.target;
+    var answer = triviaData[currentQuestion].answer;
 
-	if (selectedOption.innerHTML === answer) {
-		score++;
-		document.getElementById("result").innerHTML = "Correct!";
-	} else {
-		document.getElementById("result").innerHTML = "Wrong!";
-	}
+    if (selectedOption.innerHTML === answer) {
+	score++;
+	document.getElementById("result").innerHTML = "Correct!";
+    } else {
+	document.getElementById("result").innerHTML = "Wrong!";
+    }
 
-	currentQuestion++;
+    currentQuestion++;
 
-	if (currentQuestion < totalQuestions) {
-		displayQuestion();
-	} else {
-		document.getElementById("question").innerHTML = "Quiz completed!";
-		document.getElementById("options").innerHTML = "";
-		document.getElementById("submit").style.display = "none";
-		document.getElementById("result").innerHTML = "";
-		document.getElementById("score").innerHTML = "Your score: " + score + " out of " + totalQuestions;
-	}
+    if (currentQuestion < triviaData.length) {
+	displayQuestion();
+    } else {
+	document.getElementById("question").innerHTML = "Quiz completed!";
+	document.getElementById("options").innerHTML = "";
+	document.getElementById("submit").style.display = "none";
+	document.getElementById("result").innerHTML = "";
+	document.getElementById("score").innerHTML = "Your score: " + score + " out of " + triviaData.length;
+    }
 }
 
-// Change the argument to specify the desired number of questions
-initializeQuiz(10);
+displayQuestion();
