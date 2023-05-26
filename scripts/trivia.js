26071e003d3b86ec1e782874bfe331efb72b2680
Denis Lemire <denis@lemire.name>
2023-05-26 12:16:08 -0600

    Randomize questions

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,8 +1,6 @@
 // Trivia questions and answers
 var triviaData;
 
-var triviaData;
-
 // Function to load the JSON file
 function loadJSON(callback) {
 	var xobj = new XMLHttpRequest();
@@ -19,16 +17,22 @@ function loadJSON(callback) {
 // Load the JSON file and assign the data to the triviaData variable
 loadJSON(function(data) {
 	triviaData = data;
-	displayQuestion();
+	var numberOfQuestions = 5; // Change the number of questions here
+	displayQuestion(numberOfQuestions);
 });
 
 var currentQuestion = 0;
 var score = 0;
 
-function displayQuestion() {
+function displayQuestion(n) {
 	var questionContainer = document.getElementById("question");
 	var optionsContainer = document.getElementById("options");
-	var questionData = triviaData[currentQuestion];
+
+	// Randomly select 'n' questions from triviaData
+	var randomQuestions = getRandomQuestions(triviaData, n);
+
+	// Get the current question data from randomQuestions array
+	var questionData = randomQuestions[currentQuestion];
 
 	questionContainer.innerHTML = questionData.question;
 	optionsContainer.innerHTML = "";
@@ -41,28 +45,45 @@ function displayQuestion() {
 	}
 }
 
-function checkAnswer(event) {
-    var selectedOption = event.target;
-    var answer = triviaData[currentQuestion].answer;
+function getRandomQuestions(array, n) {
+	// Create a copy of the array to avoid modifying the original array
+	var newArray = array.slice();
 
-    if (selectedOption.innerHTML === answer) {
-	score++;
-	document.getElementById("result").innerHTML = "Correct!";
-    } else {
-	document.getElementById("result").innerHTML = "Wrong!";
-    }
+	// Randomly shuffle the array
+	for (var i = newArray.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = newArray[i];
+		newArray[i] = newArray[j];
+		newArray[j] = temp;
+	}
 
-    currentQuestion++;
-
-    if (currentQuestion < triviaData.length) {
-	displayQuestion();
-    } else {
-	document.getElementById("question").innerHTML = "Quiz completed!";
-	document.getElementById("options").innerHTML = "";
-	document.getElementById("submit").style.display = "none";
-	document.getElementById("result").innerHTML = "";
-	document.getElementById("score").innerHTML = "Your score: " + score + " out of " + triviaData.length;
-    }
+	// Return the first 'n' elements
+	return newArray.slice(0, n);
 }
 
-displayQuestion();
+function checkAnswer(event) {
+	var selectedOption = event.target;
+	var answer = triviaData[currentQuestion].answer;
+
+	if (selectedOption.innerHTML === answer) {
+		score++;
+		document.getElementById("result").innerHTML = "Correct!";
+	} else {
+		document.getElementById("result").innerHTML = "Wrong!";
+	}
+
+	currentQuestion++;
+
+	if (currentQuestion < triviaData.length) {
+		displayQuestion();
+	} else {
+		document.getElementById("question").innerHTML = "Quiz completed!";
+		document.getElementById("options").innerHTML = "";
+		document.getElementById("submit").style.display = "none";
+		document.getElementById("result").innerHTML = "";
+		document.getElementById("score").innerHTML = "Your score: " + score + " out of " + triviaData.length;
+	}
+}
+
+// Change the argument to specify the desired number of questions
+displayQuestion(10)
